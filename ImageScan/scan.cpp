8cddd86f216363824e56fd7722b3a8084406f55d
Asim Shamim <shamima@wit.edu>
2016-03-22 13:13:48 -0400

    Continuing tutorial

--- ImageScan/scan.cpp
+++ ImageScan/scan.cpp
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.p>
+#include <iostream>
+#include <sstream>
 
+using namespace std;
 using namespace cv;
 
 int main( int argc, char **argv ) {
-	
-	if( argc != 2 ) {
-		sprintf( stderr, "Not enough arugments!" );
-		exit(-1);
+	char* imageName = argv[1];
+	image = imread( imageName, 1 );	
+	if( argc != 2) {
+		fprintf(stderr, "Not enough args!" );
 	}
+	if( !image.data ) {
+		fprintf(stderr, "No image data!" );
+	}
+
 	Mat image;
+
+
 return 0;
 }
