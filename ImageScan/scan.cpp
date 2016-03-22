3c731a21224dba91b8d76d5ad1b058693719ece3
Asim Shamim <shamima@wit.edu>
2016-03-22 12:16:35 -0400

    Starting scanning tutorial

---
+++ ImageScan/scan.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main( int argc, char **argv ) {
+	
+	if( argc != 2 ) {
+		sprintf( stderr, "Not enough arugments!" );
+		exit(-1);
+	}
+	Mat image;
+return 0;
+}
