91bb3d34122185a2d9872817d072c1ad18f726ee
Asim Shamim <shamima@wit.edu>
2016-05-25 13:53:08 -0400

    Pushing new thresholding example results

--- python/ex7/simple_thresholding.py
+++ python/ex7/simple_thresholding.py
@@ -10,10 +10,17 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
-cv2.imshow("Grayed and Blurred", image)
-(T, thresh) = cv2.threshold(blurred, 125, 255, cv2.THRESH_BINARY)
-cv2.imshow("threshold binary", thresh)
-(T, threshInverse) = cv2.threshold(blurred, 125, 255, cv2.THRESH_BINARY_INV)
-cv2.imshow("Threshold binary inverse", threshInverse)
-cv2.imshow("People", cv2.bitwise_and(image, image, mask = threshInverse))
-cv2.waitKey(0)
+#cv2.imshow("Grayed and Blurred", image)
+# Lower numbers result in lower threshold
+# For brighter images, use higher thresholds
+(T, thresh) = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)
+#cv2.imshow("threshold binary", thresh)
+(T, threshInverse) = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY_INV)
+#cv2.imshow("Threshold binary inverse", threshInverse)
+finalImage = cv2.bitwise_and(image, image, mask = threshInverse)
+#cv2.waitKey(0)
+filename = input("What is this image going to be called: ")
+filename = "images/" + filename.strip() + ".jpg"
+if cv2.imwrite(filename, finalImage):
+    print("Successfully wrote ",filename)
+
