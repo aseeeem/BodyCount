ee028f4c70e44901b6496d228bc128b702ba0429
Asim Shamim <asim@revan>
2016-07-20 10:57:45 -0400

    adding video files and got video tracking working

---
+++ python/haar_detection/vidDetect.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+vid = cv2.VideoCapture('/home/asim/Documents/BodyCount/videofiles/withPeople/MVI_0046.MP4')
+face_cascade=cv2.CascadeClassifier('/home/asim/Documents/BodyCount/python/haar_detection/haarcascade_frontalface_default.xml')
+while vid.isOpened():
+    ret, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) &  0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
