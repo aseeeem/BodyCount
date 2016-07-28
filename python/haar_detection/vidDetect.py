08a9725f041313098378dcc723f4ad67efbd81d8
Asim Shamim <asim@mender>
2016-07-27 21:38:13 -0400

    Touching up files for desktop use

--- python/haar_detection/vidDetect.py
+++ python/haar_detection/vidDetect.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
+import argparse
 
-vid=cv2.VideoCapture('/home/asim/Documents/BodyCount/videofiles/withPeople/MVI_0043.MP4')
-body_cascade=cv2.CascadeClassifier('/home/asim/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_upperbody.xml')
-head_cascade=cv2.CascadeClassifier('/home/asim/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml')
-side_cascade=cv2.CascadeClassifier('/home/asim/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+ap = argparse.ArgumentParser(description='Path to video file')
+ap.add_argument('-i', '--image', help='Enter path to vido file', required=True)
+args = vars(ap.parse_args())
+vid=cv2.VideoCapture(args['image'])
+body_cascade=cv2.CascadeClassifier('./classes/haarcascade_upperbody.xml')
+head_cascade=cv2.CascadeClassifier('./classes/haarcascade_frontalface_default.xml')
+side_cascade=cv2.CascadeClassifier('./classes/haarcascade_profileface.xml')
 while vid.isOpened():
     ret, frame = vid.read()
     # Needed to downscale the video for faster processing
